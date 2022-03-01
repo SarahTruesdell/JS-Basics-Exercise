6baec522b961ff6eda1236928c2e043ba947eb4e
“SarahTruesdell” <s.truesdell87@outlook.com>
2022-03-01 10:22:48 -0500

    JS Basics Exercise

---
+++ script.js
@@ -0,0 +1,41 @@
+console.log(27);
+
+//Primitives, strings, booleans, null, undefined, and undefined
+
+//+,-,*,/,**,%
+
+console.log(5 % 2)
+console.log(5 % 4)
+
+console.log('I\'M MARY POPPINS, YA\'LL!')
+
+console.log("Hawk" + "eye")
+
+console.log("Sarah"+" "+"Truesdell")
+console.log("Sarah".length + "Truesdell".length)
+
+console.log("Avengers: Endgame"[16])
+
+//Bonus
+
+//1
+console.log("\\")
+//2
+console.log((5**4)-9)
+//3
+console.log("I".length + "Am".length % "Iron".length - "Man".length)
+//4
+console.log("Cap"[1])
+console.log("Vision"[0])
+console.log("Okoye"[4])
+console.log("Nick Fury"[0])
+console.log("Groot"[0])
+console.log("Doctor Strange"[13])
+console.log("Thor"[3])
+console.log("Star-Lord"[0])
+//5
+let m=25;
+let c=4;
+let u=m+c;
+console.log(u)
+
